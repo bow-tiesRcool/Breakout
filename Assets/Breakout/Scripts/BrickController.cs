ee1c43d3af2bc8614938f723ca7fa3cfe308f627
Evan Kiser <ekiser@institute.local>
2017-02-09 22:20:55 -0600

    Added more stuff

--- Assets/Breakout/Scripts/BrickController.cs
+++ Assets/Breakout/Scripts/BrickController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickController : MonoBehaviour {
-    
+    public int points = 10;
+
     void OnCollisionEnter(Collision c)
     {
         gameObject.SetActive(false);
+        GameManager.BrickBroken(points);
     }
        
 }
--- Assets/Breakout/Scripts/GameManager.cs
+++ Assets/Breakout/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public Text livesUI;
+    public Text scoreUI;
+    public Text highScoreUI;
+    public Text gameOverUI;
+
     public int score = 0;
     public int lives = 3;
-    void Awake()
+    public int highScore = 0;
+
+    void Awake ()
     {
         if (instance == null)
         {
@@ -19,9 +28,34 @@ public class GameManager : MonoBehaviour
             Debug.Log("Nooo");
         }
     }
-    
-    public static void LostBall()
+    void Start ()
+    {
+        livesUI.text = "Lives: " + lives;
+        scoreUI.text = "Score: " + score;
+        highScoreUI.text = "HighScore: " + highScore;
+    } 
+    public static void LostBall ()
     {
         instance.lives = instance.lives - 1;
+        instance.livesUI.text = "Lives: " + instance.lives;
+
+        if (instance.lives < 0)
+        {
+            instance.gameOverUI.text = "You Lose";
+            instance.gameOverUI.gameObject.SetActive(true);
+
+        }
+    }
+    
+    public static void BrickBroken (int points)
+    {
+        instance.score += points;
+        instance.scoreUI.text = "Score: " + instance.score;
+
+        if (instance.score > instance.highScore)
+        {
+            instance.highScore = points;
+            instance.highScoreUI.text = "HighScore: " + instance.score;
+        }
     }
 }
